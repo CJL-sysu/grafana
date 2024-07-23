fda794bb959447791f5c6b7bc2f622ec19f7dd16
CJL <2743880348@qq.com>
2024-07-23 16:39:14 +0800

    update gauge

--- gauge/init_database.sql
+++ gauge/init_database.sql
@@ -5,3 +5,13 @@ CREATE Table IF NOT EXISTS grafana.gauge(
     gpu_load DOUBLE,
     gpu_mem DOUBLE
 );
+CREATE TABLE IF NOT EXISTS grafana.memory(
+    TIME DATETIME,
+    total DOUBLE,
+    used DOUBLE
+);
+CREATE TABLE IF NOT EXISTS grafana.gpumem(
+    TIME DATETIME,
+    total DOUBLE,
+    used DOUBLE
+);
--- gauge/main.py
+++ gauge/main.py
@@ -11,12 +11,21 @@ def get_cpu_percent():
 def get_mem_percent():
     return psutil.virtual_memory().percent
 
+def get_mem_total():
+    return psutil.virtual_memory().total
+
+def get_mem_used():
+    return psutil.virtual_memory().used
+
 def get_gpu():
+    """
+    Returns: gpu load, gpu memory percentage, gpu memory used, gpu memory total, gpu temperature
+    """
     GPUs = GPUtil.getGPUs()
     if len(GPUs) == 0:
         return 0, 0
     else:
-        return GPUs[0].load, GPUs[0].memoryUtil
+        return GPUs[0].load, GPUs[0].memoryUtil, GPUs[0].memoryUsed, GPUs[0].memoryTotal, GPUs[0].temperature
     
 def main(args):
     db = database(
@@ -29,10 +38,17 @@ def main(args):
     while True:
         cpu_percent = get_cpu_percent()
         mem_percent = get_mem_percent()
-        gpu_load, gpu_mem = get_gpu()
+        gpu_load, gpu_mem_percent, gpu_mem_used, gpu_mem_total, gpu_temp = get_gpu()
         db.exec(
-            f"""INSERT INTO {args.table} (time, cpu, mem, gpu_load, gpu_mem) VALUES (NOW(), {cpu_percent}, {mem_percent}, {gpu_load}, {gpu_mem});"""
+            f"""INSERT INTO {args.database}.gauge (time, cpu, mem, gpu_load, gpu_mem) VALUES (NOW(), {cpu_percent}, {mem_percent}, {gpu_load}, {gpu_mem_percent});"""
         )
+        db.exec(
+            f"""INSERT INTO {args.database}.memory (time, total, used) VALUES (NOW(), {get_mem_total()}, {get_mem_percent()});"""
+        )
+        db.exec(
+            f"""INSERT INTO {args.database}.gpumem (time, total, used) VALUES (NOW(), {gpu_mem_total}, {gpu_mem_used});"""
+        )
+        
         sleep(args.flush)
 
 
@@ -43,7 +59,6 @@ if __name__ == "__main__":
     parser.add_argument("--user", type=str, default="node1")
     parser.add_argument("--pwd", type=str, default="mysql114514")
     parser.add_argument("--database", type=str, default="grafana")
-    parser.add_argument("--table", type=str, default="gauge")
     parser.add_argument("--log_file", type=str, default="log/transformer.log")
     parser.add_argument("--flush", type=int, default=5, help="flush interval (s)")
     args = parser.parse_args()
